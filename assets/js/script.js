cb25cfeaea4a4acb5458d1a965507bca2f034d16
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-10-15 14:21:33 +0200

    added the first milestone and declared the variables in js
    fixed some typo in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+/*
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica:
+usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra.
+La risposta finale (o output) sarà anch’essa da scrivere in console. 
+*/
+
+//dichiaro le variabili per gli elementi HTML
+const form = document.querySelector('form')
+const nameField = document.getElementById('name')
+const kmField = document.getElementById('km')
+const ageField = document.getElementById('age-passenger')
+console.log(form, nameField, kmField, ageField);
 
